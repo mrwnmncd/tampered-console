3603e886f498df829363c33e2199cdf4ad2c1242
Ripley <maewha5534@icloud.com>
2021-07-11 20:35:27 +0800

    worker method
    summons the worker method when available based on passed params

--- test/bruteforce.js
+++ test/bruteforce.js
@@ -127,6 +127,7 @@ let i = 0;
       Console.globalConsole({ fancy: true });
       // || Console.globalConsole(true);
       console.log('modified object-true-fancy global console.log');
+      console.worker('modified object-true-fancy global console.worker');
       console.warn('modified object-true-fancy global console.warn');
       console.error('modified object-true-fancy global console.error');
       console.debug('modified object-true-fancy global console.debug');
@@ -136,6 +137,7 @@ let i = 0;
       Console.globalConsole({ fancy: false });
       // || Console.globalConsole(false);
       console.log('modified object-false-fancy global console.log');
+      // console worker disabled
       console.warn('modified object-false-fancy global console.warn');
       console.error('modified object-false-fancy global console.error');
       console.debug('modified object-false-fancy global console.debug');
@@ -146,6 +148,7 @@ let i = 0;
       console.$native.log(i++);
       Console.globalConsole({ inspect: true });
       console.log('modified object-true-inspect global console.log');
+      console.worker('modified object-true-inspect global console.worker');
       console.warn('modified object-true-inspect global console.warn');
       console.error('modified object-true-inspect global console.error');
       console.debug('modified object-true-inspect global console.debug');
@@ -156,6 +159,7 @@ let i = 0;
       Console.globalConsole({ inspect: false });
       // || Console.globalConsole(true);
       console.log('modified object-false-inspect global console.log');
+      console.worker('modified object-false-inspect global console.worker');
       console.warn('modified object-false-inspect global console.warn');
       console.error('modified object-false-inspect global console.error');
       console.debug('modified object-false-inspect global console.debug');
@@ -166,6 +170,9 @@ let i = 0;
       console.$native.log(i++);
       Console.globalConsole({ namespace: true });
       console.log('ci')('modified object-true-namespace global console.log');
+      console.worker('ci')(
+        'modified object-true-namespace global console.worker',
+      );
       console.warn('ci')('modified object-true-namespace global console.warn');
       console.error('ci')(
         'modified object-true-namespace global console.error',
@@ -180,6 +187,7 @@ let i = 0;
       Console.globalConsole({ namespace: false });
       // || Console.globalConsole(true);
       console.log('modified object-false-namespace global console.log');
+      console.worker('modified object-false-namespace global console.worker');
       console.warn('modified object-false-namespace global console.warn');
       console.error('modified object-false-namespace global console.error');
       console.debug('modified object-false-namespace global console.debug');
@@ -190,6 +198,7 @@ let i = 0;
   console.$native.log(i++);
   Console.globalConsole(false);
   console.log('modified boolean-false-global console.log');
+  // console worker disabled
   console.warn('modified boolean-false-global console.warn');
   console.error('modified boolean-false-global console.error');
   console.debug('modified boolean-false-global console.debug');
@@ -210,6 +219,7 @@ let i = 0;
     Console.globalConsole({ fancy: false, inspect: false, namespace: false });
     // || Console.globalConsole(false);
     console.log('native ~ console.log');
+    // console worker disabled
     console.warn('native ~ console.warn');
     console.error('native ~ console.error');
     console.debug('native ~ console.debug');
@@ -219,6 +229,7 @@ let i = 0;
     Console.globalConsole({ fancy: true, inspect: false, namespace: false });
     // || Console.globalConsole({ fancy: true }); || Console.globalConsole(true);
     console.log('native ~ fancy ~ console.log');
+    console.worker('native ~ fancy ~ console.worker');
     console.warn('native ~ fancy ~ console.warn');
     console.error('native ~ fancy ~ console.error');
     console.debug('native ~ fancy ~ console.debug');
@@ -228,6 +239,7 @@ let i = 0;
     Console.globalConsole({ fancy: false, inspect: true, namespace: false });
     // || Console.globalConsole({ fancy: false, inspect: true });
     console.log('native ~ inspect ~ console.log');
+    // console worker disabled
     console.warn('native ~ inspect ~ console.warn');
     console.error('native ~ inspect ~ console.error');
     console.debug('native ~ inspect ~ console.debug');
@@ -238,6 +250,7 @@ let i = 0;
     /* 0 0 1 is assumed 1 0 1 */
     // || Console.globalConsole({ namespace: true });
     console.log('ci')('native ~ fancy:namespace ~ console.log');
+    console.worker('ci')('native ~ fancy:namespace ~ console.worker');
     console.warn('ci')('native ~ fancy:namespace ~ console.warn');
     console.error('ci')('native ~ fancy:namespace ~ console.eror');
     console.debug('ci')('native ~ fancy:namespace ~ console.debug');
@@ -248,6 +261,7 @@ let i = 0;
     Console.globalConsole({ fancy: true, inspect: false, namespace: true });
     // || Console.globalConsole({ namespace: true });
     console.log('ci')('native ~ fancy-namespace ~ console.log');
+    console.worker('ci')('native ~ fancy-namespace ~ console.worker');
     console.warn('ci')('native ~ fancy-namespace ~ console.warn');
     console.error('ci')('native ~ fancy-namespace ~ console.error');
     console.debug('ci')('native ~ fancy-namespace ~ console.debug');
@@ -255,7 +269,9 @@ let i = 0;
   {
     console.$native.log(i++);
     Console.globalConsole({ fancy: true, inspect: true, namespace: false });
+    // || Console.globalConsole({ inspect: true });
     console.log('native ~ fancy-inspect ~ console.log');
+    console.worker('native ~ fancy-inspect ~ console.worker');
     console.warn('native ~ fancy-inspect ~ console.warn');
     console.error('native ~ fancy-inspect ~ console.error');
     console.debug('native ~ fancy-inspect ~ console.debug');
@@ -265,6 +281,7 @@ let i = 0;
     Console.globalConsole({ fancy: false, inspect: true, namespace: true });
     /* 0 1 1 is assumed 1 1 1 */
     console.log('ci')('native ~ fancy:inspect-namespace ~ console.log');
+    console.worker('ci')('native ~ fancy:inspect-namespace ~ console.worker');
     console.warn('ci')('native ~ fancy:inspect-namespace ~ console.warn');
     console.error('ci')('native ~ fancy:inspect-namespace ~ console.error');
     console.debug('ci')('native ~ fancy:inspect-namespace ~ console.debug');
