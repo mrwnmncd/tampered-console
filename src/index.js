f5bd7798b3cebf7d409ef310612890e3f2a1ca4f
Ripley <maewha5534@icloud.com>
2021-07-12 16:49:23 +0800

    module routes
    imports of exports and confusing things

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ mrwnmncd (c) 2021
 https://github.com/mrwnmncd 
 */
 'use strict';
-const Console = require('./console');
-const { globalConsole } = require('./overrides');
+
 {
-  module.exports = Console;
+  module.exports = require('./classes/console');
+  module.exports.inspect = require('./classes/inspected-console');
+  const { globalConsole } = require('./overrides');
   module.exports.globalConsole = globalConsole;
-}
-{
-  const { log, worker, warn, error } = require('./static-console-inspect');
-  module.exports.inspect = { log, worker, warn, error };
 }
