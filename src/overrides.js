8d6eea9df12a90ab565c3b3f4cc9eee330cce041
Ripley <maewha5534@icloud.com>
2021-07-12 16:48:41 +0800

    simplified override
    removed many gibberish; use standard streams

--- src/overrides.js
+++ src/overrides.js
@@ -1,75 +1,43 @@
 'use strict';
-require('./init');
+const util = require('util');
 /**
- * @param {object|boolean} options options for global `console`
+ * Params must be boolean or string, else assumed `true`.
+ * @param {boolean} option option for global `console`
  * @example
- * globalConsole({ fancy: false, inspect: true });
- * globalConsole(true);
+ * globalConsole(true); // override
+ * globalConsole('namespace'); // global uniform namespace
+ * globalConsole({ namespace: true }); // global namespace
  */
-function globalConsole(options) {
+function globalConsole(option) {
   let overrideNative = true;
-  let autoInspect = false;
   let globalNamespace = false;
+  let autoNamespace;
   {
-    switch (typeof options) {
-      case 'object': {
-        let { fancy, inspect, namespace } = options;
-        if (fancy && typeof fancy !== 'boolean') {
-          fancy = true;
-        }
-        if (inspect && typeof inspect !== 'boolean') {
-          inspect = true;
-        }
-        if (namespace && typeof namespace !== 'boolean') {
-          namespace = false;
-        }
-        {
-          switch (fancy) {
-            case true:
-            case false:
-              fancy = fancy;
-              break;
-            default:
-              fancy = true;
-              break;
-          }
-          switch (inspect) {
-            case true:
-            case false:
-              inspect = inspect;
-              break;
-            default:
-              inspect = false;
-              break;
-          }
-          switch (namespace) {
-            case true:
-              fancy = true;
-            case false:
-              namespace = namespace;
-              break;
-            default:
-              namespace = false;
-              break;
-          }
-        }
-        {
-          overrideNative = fancy;
-          autoInspect = inspect;
-          globalNamespace = namespace;
-        }
-        break;
-      }
+    switch (typeof option) {
       case 'boolean':
-        overrideNative = options;
-        autoInspect = false;
+        overrideNative = option;
         globalNamespace = false;
         break;
+      case 'number':
       case 'string':
+        autoNamespace = option;
         overrideNative = true;
-        autoInspect = false;
         globalNamespace = true;
-        autoNamespace = options;
+        break;
+      case 'object':
+        const { namespace } = option;
+        switch (typeof namespace) {
+          case 'boolean':
+            globalNamespace = namespace;
+            break;
+          case 'string':
+          case 'number':
+            autoNamespace = option;
+            option = namespace;
+            break;
+          default:
+            break;
+        }
         break;
       default:
         overrideNative = true;
@@ -78,20 +46,35 @@ function globalConsole(options) {
   }
   {
     if (overrideNative === true) {
-      if (autoInspect === true) {
-        if (globalNamespace === true) {
+      {
+        const {
+          log,
+          worker,
+          warn,
+          error,
+          debug,
+        } = require('./functions/inspected-console');
+        console.inspect = {};
+        console.inspect.log = log;
+        console.inspect.worker = worker;
+        console.inspect.warn = warn;
+        console.inspect.error = error;
+        console.inspect.debug = debug;
+      }
+      if (globalNamespace === true) {
+        if (autoNamespace) {
           const {
             log,
             worker,
             warn,
             error,
-          } = require('./instance-console-inspect');
-          const { debug } = require('./instance-debug');
-          console.debug = debug;
-          console.log = log;
-          console.worker = worker;
-          console.warn = warn;
-          console.error = error;
+            debug,
+          } = require('./functions/instance-console');
+          console.debug = debug(autoNamespace);
+          console.log = log(autoNamespace);
+          console.worker = worker(autoNamespace);
+          console.warn = warn(autoNamespace);
+          console.error = error(autoNamespace);
           return;
         }
         const {
@@ -99,66 +82,44 @@ function globalConsole(options) {
           worker,
           warn,
           error,
-        } = require('./static-console-inspect');
-        const { debug } = require('./static-debug');
-        console.debug = debug;
-        console.log = log;
-        console.worker = worker;
-        console.warn = warn;
-        console.error = error;
-        return;
-      }
-      if (globalNamespace === true) {
-        if (typeof options === 'string') {
-          const { log, worker, warn, error } = require('./instance-console');
-          const { debug } = require('./instance-debug');
-          console.debug = debug(options);
-          console.log = log(options);
-          console.worker = worker(options);
-          console.warn = warn(options);
-          console.error = error(options);
-          return;
-        }
-        const { log, worker, warn, error } = require('./instance-console');
-        const { debug } = require('./instance-debug');
-        console.debug = debug;
-        console.log = log;
-        console.worker = worker;
-        console.warn = warn;
-        console.error = error;
-        return;
-      }
-      {
-        const { log, worker, warn, error } = require('./static-console');
-        const { debug } = require('./static-debug');
-        console.debug = debug;
-        console.log = log;
-        console.worker = worker;
-        console.warn = warn;
-        console.error = error;
-      }
-    }
-    if (!overrideNative || overrideNative === false) {
-      if (autoInspect === true) {
-        const {
-          log,
           debug,
-          warn,
-          error,
-        } = require('./static-console-just-inspect');
+        } = require('./functions/instance-console');
+        console.debug = debug;
         console.log = log;
+        console.worker = worker;
         console.warn = warn;
         console.error = error;
-        console.debug = debug;
-        delete console.worker;
         return;
       }
-      console.log = console.$native.log;
-      console.warn = console.$native.warn;
-      console.error = console.$native.error;
-      console.debug = console.$native.debug;
+      const {
+        log,
+        worker,
+        warn,
+        error,
+        debug,
+      } = require('./functions/console');
+      console.debug = debug;
+      console.log = log;
+      console.worker = worker;
+      console.warn = warn;
+      console.error = error;
+      return;
     }
   }
+  if (!overrideNative || overrideNative === false) {
+    delete console.inspect;
+    console.log = function () {
+      process.stdout.write(util.format.apply(this, arguments) + '\n');
+    };
+    console.warn = function () {
+      process.stderr.write(util.format.apply(this, arguments) + '\n');
+    };
+    console.error = function () {
+      process.stderr.write(util.format.apply(this, arguments) + '\n');
+    };
+    console.debug = function () {
+      process.stdout.write(util.format.apply(this, arguments) + '\n');
+    };
+  }
 }
-
-module.exports = { globalConsole };
+exports.globalConsole = globalConsole;
