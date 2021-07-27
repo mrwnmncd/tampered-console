df96d5f2126ce5c6d1ca5542caa3ef9058f1bf95
Ripley <maewha5534@icloud.com>
2021-07-27 13:08:10 +0800

    delete index test script
    well there's bruteforce you know

--- test/index.js
+++
@@ -1,9 +0,0 @@
-const Console = require('../src');
-{
-  Console.log('static Console.log');
-  Console.worker('static Console.worker');
-  Console.warn('static Console.warn');
-  Console.error('static Console.error');
-  Console.debug('static Console.debug');
-  Console.namespace.log('ci')('cied');
-}
